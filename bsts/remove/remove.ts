63412559d777bda464e7d8745bb2b8d5588f79ef
Cooper Spinelli <cooperspinelli@gmail.com>
2024-04-24 18:26:00 -0600

    Refactor remove

--- bsts/remove/remove.ts
+++ bsts/remove/remove.ts
@@ -1,7 +1,6 @@
 import { BNodeNum, BSTNum } from "../common/bst";
 import { findSuccessor } from "../successor/successor";
 import { find } from "../find/find";
-import { b } from "vitest/dist/suite-HPAKvIxA";
 
 function findParent(node: BNodeNum | null, val: number): BNodeNum | null {
 
@@ -17,78 +16,6 @@ function findParent(node: BNodeNum | null, val: number): BNodeNum | null {
 
   return null;
 }
-
-/** remove(bst, val): Removes a node in the BST with the value val.
- * Returns the removed node. */
-
-function removeOld(bst: BSTNum, val: number): BNodeNum {
-  const nodeToRemove = find(bst.root, val)!;
-  if (!nodeToRemove) throw new Error();
-  const parentNode = findParent(bst.root, val);
-
-  // If node to remove is the root
-  if (!parentNode) {
-
-    // If root has both children
-    if (bst.root?.right && bst.root?.left) {
-      const successor = findSuccessor(nodeToRemove)!;
-      remove(bst, successor.val);
-      successor.left = nodeToRemove.left;
-      successor.right = nodeToRemove.right;
-      bst.root = successor;
-
-      // If root has just left child
-    } else if (bst.root?.left) {
-      bst.root = bst.root?.left;
-
-      // If root has just right child
-    } else if (bst.root?.right) {
-      bst.root = bst.root!.right;
-
-      // If root has no children
-    } else {
-      bst.root = null;
-    }
-
-    return nodeToRemove;
-  }
-
-  let toSet;
-
-  // If node to remove has two children
-  if (nodeToRemove.right && nodeToRemove.left) {
-    const successor = findSuccessor(nodeToRemove)!;
-    remove(bst, successor.val);
-    successor.left = nodeToRemove.left;
-    successor.right = nodeToRemove.right;
-    toSet = successor;
-
-    // If node to remove has just left child
-  } else if (nodeToRemove.left) {
-    toSet = nodeToRemove.left;
-
-    // If node to remove has just right child or no children
-  } else {
-    toSet = nodeToRemove.right;
-  }
-
-
-  if (nodeToRemove === parentNode?.left) {
-    parentNode!.left = toSet;
-  } else {
-    parentNode!.right = toSet;
-  }
-
-
-  return nodeToRemove;
-}
-//find node and parent
-//if node is leaf, remove from parent
-//if node has one child, make child node into child of parent
-// if node has two children, call remove on successor and make found node into successor node
-// if node is root with no children, set bst.root = null
-
-
 
 /** remove(bst, val): Removes a node in the BST with the value val.
  * Returns the removed node. */
@@ -129,9 +56,5 @@ function remove(bst: BSTNum, val: number): BNodeNum {
 
   return nodeToRemove;
 }
-
-
-
-
 
 export { remove };
